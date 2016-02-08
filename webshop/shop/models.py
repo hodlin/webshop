a4eeb87649cb00221b4211e490c527afa4e5e4ea
Dmytro Domin <mr.diomin@gmail.com>
2016-02-08 17:00:10 +0200

    Orders app started to serve clients' orders

--- webshop/shop/models.py
+++ webshop/shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -6,7 +7,6 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=200,
                             db_index=True)
-
     slug = models.SlugField(max_length=200,
                             db_index=True,
                             unique=True)
@@ -23,16 +23,12 @@ class Category(models.Model):
 class Product(models.Model):
     category = models.ForeignKey(Category,
                                  related_name='products')
-
     name = models.CharField(max_length=200,
                             db_index=True)
-
     slug = models.SlugField(max_length=200,
                             db_index=True)
-
     image = models.ImageField(upload_to='products/%Y/%m/%d',
                               blank=True)
-
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.PositiveIntegerField()
