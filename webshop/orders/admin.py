336e88179c889b01f60e889d58f9b1b92fd3fb7f
Dmytro Domin <mr.diomin@gmail.com>
2016-02-10 11:23:55 +0200

    rewrited registration to admin site through decorators

--- webshop/orders/admin.py
+++ webshop/orders/admin.py
@@ -7,11 +7,10 @@ class OrderItemInline(admin.TabularInline):
     raw_id_fields = ['product']
 
 
+@admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email',
                     'address', 'postal_code', 'city', 'paid',
                     'created', 'updated']
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
-
-admin.site.register(Order, OrderAdmin)
