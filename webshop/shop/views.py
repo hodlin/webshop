c512ead49035c773f9d28af4978fe7be30f99f18
Dmytro Domin <mr.diomin@gmail.com>
2016-02-10 23:18:13 +0200

    mirgated to PostgreSQL database

--- webshop/shop/views.py
+++ webshop/shop/views.py
@@ -5,7 +5,7 @@ from .models import Category, Product
 def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
-    products = Product.objects.filte(available=True)
+    products = Product.objects.filter(available=True)
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
@@ -15,5 +15,5 @@ def product_list(request, category_slug=None):
 
 
 def product_detail(request, id, slug):
-    product = get_object_or_404(Product, id=id, slug=slug, avalable=True)
+    product = get_object_or_404(Product, id=id, slug=slug, available=True)
     return render(request, 'shop/product/detail.html', {'product': product})
