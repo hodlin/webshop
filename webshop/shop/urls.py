1c6f07dae6575c532335e2d8352bb1a45c7c7aa2
Dmytro Domin <mr.diomin@gmail.com>
2016-02-10 12:27:12 +0200

    static and media setted

--- webshop/shop/urls.py
+++ webshop/shop/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls import url
+from django.conf.urls.static import static
 from . import views
 
 
@@ -9,3 +11,7 @@ urlpatterns = [
     url(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$',
         views.product_detail, name='product_detail'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
