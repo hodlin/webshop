0cf42713546ab87af728140c028eeb231876e52c
Dmytro Domin <mr.diomin@gmail.com>
2016-02-10 23:53:06 +0200

    add() and save() methods added to the Cart class

--- webshop/cart/cart.py
+++ webshop/cart/cart.py
@@ -15,3 +15,22 @@ class Cart(object):
             # Save an empty cart to session
             cart = self.session[settings.CART_SESSION_ID] = {}
         self.cart = cart
+
+    def add(self, product, quantity=1, update_quantity=False):
+        """
+        Add a product to the cart or update its quantity
+        """
+        product_id = str(product.id)
+        if product_id not in self.cart:
+            self.cart[product_id] = {'quantity': 0,
+                                     'price': str(product.price)}
+        if update_quantity:
+            self.cart[product_id]['quantity'] = quantity
+        else:
+            self.cart[product_id]['quantity'] += quantity
+
+    def save(self):
+        # Update the session cart
+        self.session[settings.CART_SESSION_ID] = self.cart
+        # Mark the session as 'modified' to make sure it is saved
+        self.session.modified = True
