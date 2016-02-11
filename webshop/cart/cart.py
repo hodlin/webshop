4cabb1dcd19f70a95ca8e411307dbfc9a3d71867
Dmytro Domin <mr.diomin@gmail.com>
2016-02-12 01:21:44 +0200

    saving cart after additon

--- webshop/cart/cart.py
+++ webshop/cart/cart.py
@@ -28,6 +28,7 @@ class Cart(object):
             self.cart[product_id]['quantity'] = quantity
         else:
             self.cart[product_id]['quantity'] += quantity
+        self.save()
 
     def save(self):
         # Update the session cart
