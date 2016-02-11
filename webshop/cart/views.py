d194a159af61f3a01cb27cfc10fcce4cf7cd41a1
Dmytro Domin <mr.diomin@gmail.com>
2016-02-12 00:26:06 +0200

    cart_detail() view added

--- webshop/cart/views.py
+++ webshop/cart/views.py
@@ -24,3 +24,7 @@ def cart_remove(request, product_id):
     cart.remove(product)
     return redirect('cart:cart_detail')
 
+
+def cart_detail(request):
+    cart = Cart(request)
+    return render(request, 'cart/detail.html', {'cart': cart})
