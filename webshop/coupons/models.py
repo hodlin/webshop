a6c3a89fcabea503cf4b32b0c4f3c086551d6eae
Dmytro Domin <mr.diomin@gmail.com>
2016-02-28 14:30:54 +0200

    Coupon model created

--- webshop/coupons/models.py
+++ webshop/coupons/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxLengthValidator
 
-# Create your models here.
+
+class Coupon(models.Model):
+    code = models.CharField(max_length=50, unique=True)
+    valid_from = models.DateTimeField()
+    valid_to = models.DateTimeField()
+    discount = models.IntegerField(validators=[MinValueValidator(0),
+                                               MaxLengthValidator(100)])
+    active = models.BooleanField()
+
+    def __str__(self):
+        return self.code
